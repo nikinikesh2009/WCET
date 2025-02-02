f0a8b150b86f1fd2abaad2c262ef4b8ab11657a0
nikinikesh2009 <nikilnikesh2009@gmail.com>
2025-02-02 18:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
         heroText.style.textShadow = "0 0 15px cyan";
     }, 1500);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    let year = new Date().getFullYear();
+    document.getElementById("current-year").textContent = year;
+    document.getElementById("floating-year").textContent = year;
+});
